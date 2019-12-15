3a1675ec10570354d0d290b172b58aaf313b6784
wpbgent <shanahan.bjs@gmail.com>
2019-12-14 19:28:59 -0500

    moved to ver99

--- test/test.js
+++ test/test.js
@@ -5,6 +5,6 @@ describe('GET /', function() {
  it('respond with hello world', function(done) {
 
  //navigate to root and check the the response is "hello world"
- request(app).get('/').expect('hello ver9', done);
+ request(app).get('/').expect('hello ver99', done);
  });
 });
